d642eed9a463632075950a9e97a0d6903513c11e
Sam Bryce <sbryce@clemson.edu>
2013-12-12 23:59:25 -0500

    added initial specs

---
+++ specs/notePatterns/fifths.lua
@@ -0,0 +1,14 @@
+notes = {
+  {beat = 1, angle = 0},
+  {beat = 2, angle = 1},
+  {beat = 3, angle = 0},
+  {beat = 4, angle = 1},
+  {beat = 5, angle = 2},
+  {beat = 6, angle = 3},
+  {beat = 7, angle = 2},
+  {beat = 8, angle = 3},
+  {beat = 9, angle = 0},
+  {beat = 10, angle = 1},
+  {beat = 11, angle = 0},
+  {beat = 12, angle = 1}
+}
