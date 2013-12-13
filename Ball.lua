1a439cec5f006840751010076aaf599ba02802c7
Sam Bryce <sbryce@clemson.edu>
2013-12-12 21:23:13 -0500

    adding everything I have so far

---
+++ Ball.lua
@@ -0,0 +1,36 @@
+require 'class'
+vector = require 'hump.vector'
+
+Ball = newclass("Ball")
+
+function Ball:init(player, pos, vel, beat, radius)
+  self.player = player
+  self.pos = pos
+  self.vel = vel
+  self.active = true
+  self.prevPos = pos
+  self.collisionTime = collisionTime
+  self.beat = beat
+  self.radius = radius
+  self.willHit = false
+end
+
+function Ball:update(dt)
+  local dir = self.player.pos - self.pos
+  dir:normalize_inplace()
+  self.pos = self.pos + self.vel * dir * dt
+  local distFromCenter = math.abs(self.player.pos:dist(self.pos))
+  local prevDistFromCenter = math.abs(self.player.pos:dist(self.prevPos))
+  if prevDistFromCenter > self.player.radius and distFromCenter < self.player.radius then
+    if self.player:isBlocked(self.pos) then
+      self.active = false
+    else
+      self.willHit = true
+    end
+  end
+  self.prevPos = self.pos
+end
+
+function Ball:draw()
+  love.graphics.circle("fill", self.pos.x, self.pos.y, self.radius, 20)
+end
---
+++ Player.lua
@@ -0,0 +1,46 @@
+require 'class'
+vector = require 'hump.vector'
+
+Player = newclass("Player")
+
+function Player:init(pos)
+  self.pos = pos
+  self.startAngle = 1
+  self.radius = 50
+  self.angle = 0.25 * math.pi
+  self.angularVelocity = 8
+end
+
+function Player:update(dt)
+  if love.keyboard.isDown("right") then
+    self.startAngle = (self.startAngle + self.angularVelocity * dt) % (2 * math.pi)
+  end
+  if love.keyboard.isDown("left") then
+    self.startAngle = (self.startAngle - self.angularVelocity * dt) % (2 * math.pi)
+  end
+end
+
+function Player:draw()
+  love.graphics.arc("line", self.pos.x, self.pos.y, self.radius, self.startAngle, self.startAngle + self.angle)
+end
+
+function Player:isBlocked(vec)
+    local centerToPoint = vec - self.pos
+    centerToPoint:normalize_inplace()
+    local vecAngle
+    if centerToPoint:cross(vector(1, 0)) > 0 then
+      vecAngle = 2 * math.pi - math.acos(centerToPoint * vector(1, 0))
+    else
+      vecAngle = math.acos(centerToPoint * vector(1, 0))
+    end
+    --Handle the case where the shield is straddling the 0/2pi rotation line 
+    if self.startAngle + self.angle > 2 * math.pi then
+      if vecAngle > self.startAngle or vecAngle < self.startAngle + self.angle - (2 * math.pi) then
+        return true
+      end
+    end
+    if vecAngle > self.startAngle and vecAngle < self.startAngle + self.angle then
+      return true
+    end
+  return false
+end
