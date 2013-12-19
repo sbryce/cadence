96325824227bcc32bfa583ab0ad66564a9929333
Sam Bryce <sbryce@clemson.edu>
2013-12-18 23:20:44 -0500

    now takes in a player object

--- Obstacle.lua
+++ Obstacle.lua
@@ -1,11 +1,22 @@
 require 'class'
+require 'collision'
+require 'gameOver'
 
 Obstacle = newclass("Obstacle")
 
-function Obstacle:init()
-  self.height = 10
-  self.width = 10
-  self.pos = vector(500, 310 - self.height)
+function Obstacle:init(player)
+  self.height = 20
+  self.width = 20
+  self.pos = vector(1000, 310 - self.height)
+  self.vel = -4
+  self.player = player
+end
+
+function Obstacle:update()
+  self.pos.x = self.pos.x + self.vel
+  if circleRecCollision(self.player.ballPos, self.player.radius, self.pos, self.width, self.height) then
+    Gamestate.switch(gameOver)
+  end
 end
 
 function Obstacle:draw()
