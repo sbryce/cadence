f1590edda3205719d050a553b75b6a3028ae5124
Sam Bryce <sbryce@clemson.edu>
2013-12-18 23:53:15 -0500

    changed constructor

--- Obstacle.lua
+++ Obstacle.lua
@@ -4,17 +4,18 @@ require 'gameOver'
 
 Obstacle = newclass("Obstacle")
 
-function Obstacle:init(player)
+function Obstacle:init(x, width, vel, player)
   self.height = 20
-  self.width = 20
-  self.pos = vector(1000, 310 - self.height)
-  self.vel = -4
+  self.width = width
+  self.pos = vector(x, 310 - self.height)
+  self.vel = -vel
   self.player = player
 end
 
-function Obstacle:update()
-  self.pos.x = self.pos.x + self.vel
+function Obstacle:update(dt)
+  self.pos.x = self.pos.x + self.vel * dt
   if circleRecCollision(self.player.ballPos, self.player.radius, self.pos, self.width, self.height) then
+    love.audio.stop()
     Gamestate.switch(gameOver)
   end
 end
