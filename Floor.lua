8a2e67c1c458dba9db2f660d7015495c7fc8ff79
Sam Bryce <sbryce@clemson.edu>
2013-12-18 23:52:33 -0500

    Floor now does not handle obstacles and only draws itself

--- Floor.lua
+++ Floor.lua
@@ -4,16 +4,16 @@ require 'Obstacle'
 Floor = newclass("Floor")
 
 function Floor:init(player)
-  self.obst = Obstacle(player)
+  --self.obst = Obstacle(player)
   self.player = player
 end
 
 function Floor:update(dt)
-  self.obst:update(dt)
+  --self.obst:update(dt)
 end
 
 function Floor:draw()
   love.graphics.setColor(20, 20, 20)
   love.graphics.rectangle("fill", 0, 310, 1000, 800)
-  self.obst:draw()
+  --self.obst:draw()
 end
